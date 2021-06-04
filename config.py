af5b7f6aef6f9736f10fe738be779c241cad9d8c
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-04 18:03:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 
-bot = TelegramClient('bot', 5581574,3a521d96e37a33434633d947f6ed9ee8).start(bot_token=1892981260:AAG4wCUQp-SES4YleNeT4lQoXCid0k4Fajg)
+bot = TelegramClient('bot', api_id,api_hash).start(bot_token=BOT_TOKEN)
