421c467a40abaa23c1645c18d04b29669c208b35
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-04 18:08:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
-from telethon import TelegramClient
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+"""HerokuAnimeDLBot CREDENTIALS"""
 
-bot = TelegramClient('bot', api_id,api_hash).start(bot_token=BOT_TOKEN)
+api_id = os.environ.get("api_id", None)
+api_hash = os.environ.get("api_hash", None)
+bot_token = os.environ.get("bot_token", None)
