59e67981b37efe07b63b8f99fff8781974836923
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-06-04 17:22:30 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -5,7 +5,9 @@ Use /help to know commands and how to use this bot'''
 help_text = '''List of commands:
 /manga <name of Manga you want> : to download any Manga by search
 
-/nh : to get nh manga
+/nh : <name of Adult Manga you want>
+
+/read : <name of Manga you want to read>
                                
 The links provided are in multiple qualities to download just open links in chrome and download starts automatically
                                
