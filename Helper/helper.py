ddcf084faf2704113092ff77fd9775268b9d48df
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-08-01 19:41:24 +0530

    Update helper.py

--- Helper/helper.py
+++ Helper/helper.py
@@ -1,7 +1,5 @@
 start_text = '''Welcome to Manga DL Bot. Here you can Download all Mangas for Free.
 
-You can Also download Anime for free at @tganimesterambot.
-
 Please read the Instructions carefully before surfing in...
 
 Use /help to know commands and how to use this bot'''
@@ -10,11 +8,9 @@ help_text = '''List of commands:
 
 /manga <name of Manga you want> : to download any Manga by search
 
-/nh : </nh <hentai code>
-
 /read : <name of Manga you want to read>
                                
 The files provided are in multiple qualities to download just open filein chrome
                                
-To report any Problems, Bugs, Suggestions go to @Animemusicarchive6'''
+To report any Problems, Bugs, Suggestions go to @AsmSafone'''
 
