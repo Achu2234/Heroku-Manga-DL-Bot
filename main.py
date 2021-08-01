5fea4dba41f0a4a2fdbc76bd0ccf8d744d37958f
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-08-01 19:46:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from Plugins.starter import start
 from Plugins.manga import Manga
-from Plugins.nhentai import Nhentai
 from config import bot
 
 try:
     start()
     Manga()
-    Nhentai()
     
 except Exception as e:
     print(e)
